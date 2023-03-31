09d99034c7f2d1ab8680eaedbcd07c5531bfd6c8
christopher_gomez <christopher_gomez@wayforward.com>
2023-03-31 15:04:29 -0700

    Added env var creation to pipeline

--- server.js
+++ server.js
@@ -1,34 +1,44 @@
 /* eslint-disable */
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const morgan = require('morgan');
-const serveStatic = require('serve-static')
-const path = require('path')
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const morgan = require("morgan");
+const serveStatic = require("serve-static");
+const path = require("path");
 var config;
-if (process.env.NODE_ENV !== 'production') {
-	config = require('./config/settings');
+if (process.env.NODE_ENV !== "production") {
+  config = require("./config/settings");
+} else {
+  require("./createEnv");
 }
 //var history = require('connect-history-api-fallback');
 
 // Set up the app
 const app = express();
-var server = require('http').createServer(app);
-var io = require('socket.io')(server);
-app.use(morgan('combined'));
+var server = require("http").createServer(app);
+var io = require("socket.io")(server);
+app.use(morgan("combined"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
-const spotifyRoutes = require('./expressRoutes/spotifyRoutes.js')(app, io);
-app.use('/spotify', spotifyRoutes);
+const spotifyRoutes = require("./expressRoutes/spotifyRoutes.js")(app, io);
+app.use("/spotify", spotifyRoutes);
 // If not accessing the API, serve up the frontend
-app.use("/", serveStatic(path.join(__dirname, "/dist"), { etag: false, lastModified: false }));
+app.use(
+  "/",
+  serveStatic(path.join(__dirname, "/dist"), {
+    etag: false,
+    lastModified: false,
+  })
+);
 // Catch all routes and redirect to the index file
 app.get("*", (req, res) => {
   res.set({ "Content-Type": "text/html" });
-  res.sendFile(path.join(__dirname, "/dist/index.html"), { etag: false, lastModified: false });
+  res.sendFile(path.join(__dirname, "/dist/index.html"), {
+    etag: false,
+    lastModified: false,
+  });
 });
-
 
 /*// Middleware for serving '/dist' directory
 const staticFileMiddleware = express.static('dist');
@@ -47,4 +57,6 @@ app.use(staticFileMiddleware);*/
 // Listen for connections to the port
 const port = process.env.PORT || config.serverPort;
 var baseURL = process.env.baseURL || config.baseURL;
-server.listen(port, () => console.log('Server listening on port ' + baseURL + port));
+server.listen(port, () =>
+  console.log("Server listening on port " + baseURL + port)
+);
