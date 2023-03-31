403621880339443a8c796d65816332359d5accb6
christopher_gomez <christopher_gomez@wayforward.com>
2023-03-31 02:42:41 -0700

    Update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,29 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
-  'extends': [
-    'plugin:vue/essential',
-    'eslint:recommended'
-  ],
+  extends: ["plugin:vue/essential", "eslint:recommended"],
   parserOptions: {
-    ecmaVersion: 2020
+    ecmaVersion: 2020,
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
-}
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-mixed-spaces-and-tabs": "off",
+    "no-unused-vars": "off",
+    "vue/no-unused-component": "off",
+    "vue/no-reserved-keys": "off",
+    "no-undef": "off",
+    "vue/multi-word-component-names": "off",
+    "no-redeclare": "off",
+    "vue/no-mutating-props": "off",
+    "no-case-declarations": "off",
+    "vue/no-dupe-keys": "off",
+    "vue/no-arrow-functions-in-watch": "off",
+    "no-empty": "off",
+    "no-constant-condition": "off",
+    "no-dupe-keys": "off",
+    "vue/no-unused-components": "off"
+  },
+};
